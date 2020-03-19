9357c7fa2c0448edc53c5898e68d79e1bae56500
fdromero32 <60331306+fdromero32@users.noreply.github.com>
2020-03-18 20:52:37 -0700

    second commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,28 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import BusinessList from '../Business List/BusinessList'
 import SearchBar from '../Search Bar/SearchBar'
-
-var business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'Lucas pizza',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
-const businesses = [business, business, business, business, business, business]
+import Yelp from '../../util/Yelp'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      businesses: []
+    }
+
+    this.searchYelp = this.searchYelp.bind(this)
+  }
+  searchYelp(term, location, sortBy){
+    Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+      this.setState({ businesses: businesses})
+    })
+  }
+
   render() {
   return (
-      <div class="App">
-          <h1>ravenous</h1>
-          <SearchBar />
-          <BusinessList businesses={businesses}/> 
+      <div className="App">
+          <h1>yummy</h1>
+          <SearchBar searchYelp={this.searchYelp} />
+          <BusinessList businesses={this.state.businesses}/> 
       </div>
    )
   }
